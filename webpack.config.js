6c0be42a672d0d5662f67e04b8fb0c4488d0d368
muhammadyousufhere <khanyousufmy80@gmail.com>
2022-10-04 14:10:30 +0500

    webpack dev setup initialized

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    target: 'web',
+    devtool: 'source-map',
+    entry: path.join(__dirname, '/src/index.js'),
+    devServer: {
+        port: 3000,
+        open: true,
+        compress: true,
+        historyApiFallback: true,
+
+        proxy: {
+            '/api': 'http://localhost:5000',
+        },
+    },
+    output: {
+        clean: true,
+        publicPath: '/',
+        filename: '[name].bundle.js',
+        chunkFilename: '[id].[contenthash].js',
+        path: path.resolve(__dirname, '/build'),
+        assetModuleFilename: 'assets/images/[name][ext]',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(s*)css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(ttf|woff|woff2|otf)$/,
+                use: {
+                    loader: 'file-loader',
+                },
+            },
+            {
+                test: /\.(svg|jpg|png|gif|jpeg)$/i,
+                use: {
+                    loader: 'file-loader',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+            minify: {
+                removeComments: true,
+                removeAttributeQuotes: true,
+                removeTagWhitespace: true,
+            },
+        }),
+        new EslintPlugin({
+            extensions: ['jsx,js'],
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            cleanAfterEveryBuildPatterns: [
+                '**/*',
+                path.join(process.cwd(), 'public/**/*'),
+            ],
+        }),
+    ],
+};
